a7b853c68a47efa0117580d4c44145fe8bc74ca8
Joshua Wyatt Smith <joshua.wyatt.smith@cern.ch>
2019-06-13 12:36:23 +0200

    rename ui main file

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -5,7 +5,7 @@ import sys
 
 if __name__ == '__main__':
     appctxt = ApplicationContext()       # 1. Instantiate ApplicationContext
-    ui_base = appctxt.get_resource('ui_Labeller.ui')
+    ui_base = appctxt.get_resource('ui_Cedar.ui')
     ui_settings = appctxt.get_resource('ui_Settings.ui')
     mainWin = MainWindow(ui_base,ui_settings)
     mainWin.show()
