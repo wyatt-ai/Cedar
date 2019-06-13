0cf63420e3ecb6867505f20244a9d41bcf14eef5
Joshua Wyatt Smith <joshua.wyatt.smith@cern.ch>
2019-06-13 16:49:50 +0200

    make selected label column editable and change its colour

--- src/main/python/pandasModel.py
+++ src/main/python/pandasModel.py
@@ -1,9 +1,13 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt,QAbstractItemModel
+from PyQt5.QtGui import QBrush,QColor
 import pandas as pd
 
 class PandasModel(QtCore.QAbstractTableModel): 
-    def __init__(self, df = pd.DataFrame(), parent=None): 
+
+    def __init__(self, df = pd.DataFrame(),editable_column=False, parent=None): 
         QtCore.QAbstractTableModel.__init__(self, parent=parent)
+        self.editable_column = editable_column
         self._df = df
 
     def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
@@ -17,19 +21,32 @@ class PandasModel(QtCore.QAbstractTableModel):
                 return QtCore.QVariant()
         elif orientation == QtCore.Qt.Vertical:
             try:
-                # return self.df.index.tolist()
                 return self._df.index.tolist()[section]
             except (IndexError, ):
                 return QtCore.QVariant()
 
-    def data(self, index, role=QtCore.Qt.DisplayRole):
-        if role != QtCore.Qt.DisplayRole:
-            return QtCore.QVariant()
-
+    def data(self, index, role):
         if not index.isValid():
             return QtCore.QVariant()
-
+        elif role == Qt.BackgroundRole:
+            if self.editable_column!=False:
+                if index.column() == self.editable_column:
+                    return QBrush(QColor(152,251,152))
+        elif role != Qt.DisplayRole:
+            return QtCore.QVariant()
         return QtCore.QVariant(str(self._df.ix[index.row(), index.column()]))
+
+
+    def flags(self, index):
+        flags = super(PandasModel, self).flags(index)
+
+        if self.editable_column!=False:
+            if index.column() in (int(self.editable_column),):
+                flags |= Qt.ItemIsEditable
+            else:
+                flags &= Qt.ItemIsSelectable
+        return flags
+
 
     def setData(self, index, value, role):
         row = self._df.index[index.row()]
@@ -42,7 +59,7 @@ class PandasModel(QtCore.QAbstractTableModel):
             dtype = self._df[col].dtype
             if dtype != object:
                 value = None if value == '' else dtype.type(value)
-        self._df.set_value(row, col, value)
+        self._df.at[row,col]=value
         return True
 
     def rowCount(self, parent=QtCore.QModelIndex()): 
